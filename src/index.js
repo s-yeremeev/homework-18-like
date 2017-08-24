e9e2db8404af6929537d115b8f63df508aec9bf8
Sasha Yeremeev <c3pions@gmail.com>
2017-08-24 23:54:12 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom"
+import React from "react"
+import PageContainer from "./page"
+
+ReactDOM.render(
+    <PageContainer/>,
+    document.getElementById("page-structure")
+)
---
+++ src/page/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom"
+import React from "react"
+import PostContainer from "./post"
+import LikeComponent from "./like"
+
+
+export default class PageContainer extends React.PureComponent {
+    
+}
